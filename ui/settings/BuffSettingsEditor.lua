3830c9e517c89c8759c93c8af087745568c595a0
cyrite <123847593+cyrite@users.noreply.github.com>
2024-07-01 20:16:55 -0700

    Reverting change that causes buffs not to save

--- ui/settings/BuffSettingsEditor.lua
+++ ui/settings/BuffSettingsEditor.lua
@@ -35,7 +35,7 @@ function BuffSettingsEditor.new(trustSettings, buffs, targets)
     self:setScrollEnabled(true)
 
     self.trustSettings = trustSettings
-    self.buffs = (buffs or L{}):compact_map()
+    self.buffs = buffs or L{}
     self.targets = targets
     self.menuArgs = {}
 
