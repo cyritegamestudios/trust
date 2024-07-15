a0d4d5281870f83ca0cd7f7c4719be2c83e68849
cyrite <123847593+cyrite@users.noreply.github.com>
2024-07-14 18:14:32 -0700

    Adding Sentinel to Paladin default job settings

--- cylibs/trust/data/PLD.lua
+++ cylibs/trust/data/PLD.lua
@@ -21,7 +21,7 @@ function PaladinTrust.new(settings, action_queue, battle_settings, trust_setting
 		MagicBurster.new(action_queue, trust_settings.NukeSettings, 0.8, L{}, job),
 		Nuker.new(action_queue, trust_settings.NukeSettings, 0.8, L{}, job),
 		Puller.new(action_queue, battle_settings.targets, L{ Spell.new('Flash'), Spell.new('Banish') }:compact_map()),
-		Tank.new(action_queue, L{ 'Sentinel' }, L{ Spell.new('Flash') })
+		Tank.new(action_queue, L{}, L{ Spell.new('Flash') })
 	}
 	local self = setmetatable(Trust.new(action_queue, roles, trust_settings, job), PaladinTrust)
 
