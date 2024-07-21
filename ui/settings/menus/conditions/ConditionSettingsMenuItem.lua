28d21c165644028bbe0c312dc8f180c2916d2a0d
cyrite <123847593+cyrite@users.noreply.github.com>
2024-07-20 21:35:28 -0700

    Adding HasRunesCondition

--- ui/settings/menus/conditions/ConditionSettingsMenuItem.lua
+++ ui/settings/menus/conditions/ConditionSettingsMenuItem.lua
@@ -39,6 +39,7 @@ function ConditionSettingsMenuItem.new(trustSettings, trustSettingsMode, parentM
         [MainJobCondition.__type] = "main_job",
         [ReadyAbilityCondition.__type] = "ready_ability",
         [FinishAbilityCondition.__type] = "finish_ability",
+        [HasRunesCondition.__type] = "has_runes",
         --[ModeCondition.__type] = "mode", -- Need to dynamically reload mode values when mode name config cell changes
     }
 
