65b65940d367a273f1d11cd951e5a34e9d854444
cyrite <123847593+cyrite@users.noreply.github.com>
2024-08-17 21:21:36 -0700

    Bumping Trust version to 10.4.6

--- Trust.lua
+++ Trust.lua
@@ -1,7 +1,7 @@
 _addon.author = 'Cyrite'
 _addon.commands = {'Trust','trust'}
 _addon.name = 'Trust'
-_addon.version = '10.4.5'
+_addon.version = '10.4.6'
 _addon.release_notes = [[
 This update introduces new menus for Bard, autocomplete for Trust
 commands and important bug fixes for users running the Japanese client.
