dd93d2548b21cd55daf4369add76b0eac7859ed0
cyrite <123847593+cyrite@users.noreply.github.com>
2024-09-04 21:03:23 -0700

    Fixing duplicate MaxDistanceCondition on RangedAttack

--- cylibs/battle/ranged_attack.lua
+++ cylibs/battle/ranged_attack.lua
@@ -17,7 +17,12 @@ function RangedAttack.new(conditions)
     local self = setmetatable({}, RangedAttack)
     self.conditions = conditions or L{}
 
-    self:add_condition(MaxDistanceCondition.new(20))
+    local matches = (conditions or L{}):filter(function(c)
+        return c.__class == MaxDistanceCondition.__class
+    end)
+    if matches:length() == 0 then
+        self:add_condition(MaxDistanceCondition.new(20))
+    end
 
     return self
 end
