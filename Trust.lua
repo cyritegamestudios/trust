e954da370e05cab1c6f3ad7ff444b80597dd9f83
cyrite <123847593+cyrite@users.noreply.github.com>
2024-10-14 14:31:53 -0700

    Adding some arrows and bullets to cylibs/res/items.lua

--- Trust.lua
+++ Trust.lua
@@ -1,7 +1,7 @@
 _addon.author = 'Cyrite'
 _addon.commands = {'Trust','trust'}
 _addon.name = 'Trust'
-_addon.version = '11.2.1'
+_addon.version = '11.2.2'
 _addon.release_notes = [[
 This update introduces new menus for Bard, autocomplete for Trust
 commands, new commands and important bug fixes for users running the
