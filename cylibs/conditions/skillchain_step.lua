c0fa850dce17a3c05f499e1b04c5e011e7f40e7d
cyrite <123847593+cyrite@users.noreply.github.com>
2024-10-13 19:43:43 -0700

    Adding skillchain window and step conditions

---
+++ cylibs/conditions/skillchain_step.lua
@@ -0,0 +1,65 @@
+---------------------------
+-- Condition checking whether the skillchain is a specific step.
+-- @class module
+-- @name SkillchainStepCondition
+local ConfigItem = require('ui/settings/editors/config/ConfigItem')
+local PickerConfigItem = require('ui/settings/editors/config/PickerConfigItem')
+local serializer_util = require('cylibs/util/serializer_util')
+
+local Condition = require('cylibs/conditions/condition')
+local SkillchainStepCondition = setmetatable({}, { __index = Condition })
+SkillchainStepCondition.__index = SkillchainStepCondition
+SkillchainStepCondition.__class = "SkillchainStepCondition"
+SkillchainStepCondition.__type = "SkillchainStepCondition"
+
+function SkillchainStepCondition.new(step_num, operator)
+    local self = setmetatable(Condition.new(), SkillchainStepCondition)
+    self.step_num = step_num or 1
+    self.operator = operator or Condition.Operator.GreaterThanOrEqualTo
+    return self
+end
+
+function SkillchainStepCondition:is_satisfied(target_index)
+    local player = player.party:get_player()
+    if player then
+        local enemy = player.party:get_target_by_index(player:get_target_index())
+        if enemy then
+            local skillchain = enemy:get_skillchain()
+            if skillchain then
+                return self:eval(skillchain:get_step(), self.step_num, self.operator)
+            end
+        end
+    end
+    return false
+end
+
+function SkillchainStepCondition:get_config_items()
+    return L{
+        ConfigItem.new('duration', 0, 6, 1, function(value) return value.."" end, "Skillchain Step"),
+        PickerConfigItem.new('operator', self.operator, L{ Condition.Operator.GreaterThanOrEqualTo, Condition.Operator.Equals, Condition.Operator.GreaterThan, Condition.Operator.LessThan, Condition.Operator.LessThanOrEqualTo }, nil, "Operator")
+    }
+end
+
+function SkillchainStepCondition:tostring()
+    return "Skillchain step is "..self.operator.." "..self.step_num
+end
+
+function SkillchainStepCondition.description()
+    return "Skillchain step."
+end
+
+function SkillchainStepCondition.valid_targets()
+    return S{ Condition.TargetType.Self, Condition.TargetType.Enemy }
+end
+
+function SkillchainStepCondition:serialize()
+    return "SkillchainStepCondition.new(" .. serializer_util.serialize_args(self.step_num, self.operator) .. ")"
+end
+
+function SkillchainStepCondition:__eq(otherItem)
+    return otherItem.__class == SkillchainStepCondition.__class
+            and self.step_num == otherItem.step_num
+            and self.operator == otherItem.operator
+end
+
+return SkillchainStepCondition
--- ui/settings/menus/conditions/ConditionSettingsMenuItem.lua
+++ ui/settings/menus/conditions/ConditionSettingsMenuItem.lua
@@ -58,6 +58,8 @@ function ConditionSettingsMenuItem.new(trustSettings, trustSettingsMode, parentM
         [IsAlterEgoCondition.__type] = "is_alter_ego",
         [ReadyChargesCondition.__type] = "ready_charges",
         [ItemCountCondition.__type] = "item_count",
+        [SkillchainWindowCondition.__type] = "skillchain_window",
+        [SkillchainStepCondition.__type] = "skillchain_step"
     }
     self.conditionPickerItems = L(self.editableConditionClasses:keyset()):filter(function(c)
         local conditionClass = self:getFileForCondition(c)
