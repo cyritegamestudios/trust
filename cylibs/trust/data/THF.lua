c8b9ee3be5bcd6f312b988958142eaa46103dd17
cyrite <123847593+cyrite@users.noreply.github.com>
2024-11-17 14:22:30 -0800

    Adding Thief job

--- cylibs/trust/data/THF.lua
+++ cylibs/trust/data/THF.lua
@@ -5,13 +5,14 @@ ThiefTrust.__index = ThiefTrust
 local Approach = require('cylibs/battle/approach')
 local Dispeler = require('cylibs/trust/roles/dispeler')
 local Puller = require('cylibs/trust/roles/puller')
+local Thief = require('cylibs/entity/jobs/THF')
 
 function ThiefTrust.new(settings, action_queue, battle_settings, trust_settings)
 	local roles = S{
 		Dispeler.new(action_queue, L{}, L{ JobAbility.new('Steal') }, false),
 		Puller.new(action_queue, trust_settings.PullSettings.Targets, L{ Approach.new() })
 	}
-	local self = setmetatable(Trust.new(action_queue, roles, trust_settings), ThiefTrust)
+	local self = setmetatable(Trust.new(action_queue, roles, trust_settings, Thief.new()), ThiefTrust)
 
 	self.settings = settings
 	self.action_queue = action_queue
