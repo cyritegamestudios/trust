3eecd1a1b6dbd07e38be0549b65ab88237b96957
cyrite <123847593+cyrite@users.noreply.github.com>
2024-11-22 15:44:11 -0800

    Adding MagicBurstCommands to edit element blacklist

--- commands/MagicBurstCommands.lua
+++ commands/MagicBurstCommands.lua
@@ -1,3 +1,5 @@
+local element_util = require('cylibs/util/element_util')
+local PickerConfigItem = require('ui/settings/editors/config/PickerConfigItem')
 local TrustCommands = require('cylibs/trust/commands/trust_commands')
 local MagicBurstTrustCommands = setmetatable({}, {__index = TrustCommands })
 MagicBurstTrustCommands.__index = MagicBurstTrustCommands
@@ -21,6 +23,19 @@ function MagicBurstTrustCommands.new(trust, trust_settings, action_queue)
     self:add_command('light', function(_) return self:handle_toggle_mode('AutoMagicBurstMode', 'Light', 'Off')  end, 'Magic burst with light spells')
     self:add_command('dark', function(_) return self:handle_toggle_mode('AutoMagicBurstMode', 'Dark', 'Off')  end, 'Magic burst with dark spells')
 
+    -- Blacklist
+    local elements = L{ 'fire', 'ice', 'wind', 'earth', 'lightning', 'water', 'light', 'dark' }
+    self:add_command('toggle', self.handle_toggle_element, 'Toggle an element for magic bursting', L{
+        PickerConfigItem.new('element_name', elements[1], elements, function(v) return v:gsub("^%l", string.upper) end, "Element"),
+    })
+    self:add_command('enable', self.handle_enable_element, 'Remove an element from the blacklist for magic bursting', L{
+        PickerConfigItem.new('element_name', elements[1], elements, function(v) return v:gsub("^%l", string.upper) end, "Element"),
+    })
+    self:add_command('disable', self.handle_disable_element, 'Blacklist an element for magic bursting', L{
+        PickerConfigItem.new('element_name', elements[1], elements, function(v) return v:gsub("^%l", string.upper) end, "Element"),
+    })
+    self:add_command('reset', self.handle_reset_blacklist, 'Re-enable all elements')
+
     return self
 end
 
@@ -36,6 +51,10 @@ function MagicBurstTrustCommands:get_settings()
     return self.trust_settings:getSettings()[state.MainTrustSettingsMode.value]
 end
 
+function MagicBurstTrustCommands:get_valid_elements()
+    return S{ 'earth', 'lightning', 'water', 'fire', 'ice', 'wind', 'light', 'dark' }
+end
+
 -- // trust mb [auto, earth, lightning, water, fire, ice, wind, light, dark]
 function MagicBurstTrustCommands:handle_toggle_mode(mode_var_name, on_value, off_value)
     local success = true
@@ -47,6 +66,102 @@ function MagicBurstTrustCommands:handle_toggle_mode(mode_var_name, on_value, off
     else
         handle_set(mode_var_name, on_value)
     end
+
+    return success, message
+end
+
+-- // trust mb toggle [earth|lightning|water|fire|ice|wind|light|dark]
+function MagicBurstTrustCommands:handle_toggle_element(_, element_name)
+    local success
+    local message
+
+    if self:get_valid_elements():contains(element_name) then
+        element_name = element_name:gsub("^%l", string.upper)
+
+        local current_settings = self.trust_settings:getSettings()[state.MainTrustSettingsMode.value].NukeSettings
+
+        local blacklist = S(current_settings.Blacklist)
+        local is_enabled = blacklist:contains(element_util[element_name])
+        if is_enabled then
+            blacklist:remove(element_util[element_name])
+
+            success = true
+            message = "Magic bursts of type "..element_name.." are enabled"
+        else
+            blacklist:add(element_util[element_name])
+
+            success = true
+            message = "Magic bursts of type "..element_name.." are disabled"
+        end
+
+        current_settings.Blacklist = L(blacklist)
+
+        self.trust_settings:saveSettings(true)
+    else
+        success = false
+        message = "Invalid element "..(element_name or 'nil')..", valid elements are "..self:get_valid_elements():tostring()
+    end
+
+    return success, message
+end
+
+-- // trust mb enable [earth|lightning|water|fire|ice|wind|light|dark]
+function MagicBurstTrustCommands:handle_enable_element(_, element_name)
+    return self:handle_set_element(element_name, true)
+end
+
+-- // trust mb disable [earth|lightning|water|fire|ice|wind|light|dark]
+function MagicBurstTrustCommands:handle_disable_element(_, element_name)
+    return self:handle_set_element(element_name, false)
+end
+
+function MagicBurstTrustCommands:handle_set_element(element_name, enabled)
+    local success
+    local message
+
+    if self:get_valid_elements():contains(element_name) then
+        element_name = element_name:gsub("^%l", string.upper)
+
+        local current_settings = self.trust_settings:getSettings()[state.MainTrustSettingsMode.value].NukeSettings
+
+        local blacklist = L(current_settings.Blacklist)
+        if enabled then
+            blacklist = blacklist:filter(function(e) return e:get_name() ~= element_name end)
+
+            success = true
+            message = "Magic bursts of type "..element_name.." are enabled"
+        else
+            if not blacklist:contains(element_util[element_name]) then
+                blacklist:append(element_util[element_name])
+            end
+
+            success = true
+            message = "Magic bursts of type "..element_name.." are disabled"
+        end
+
+        current_settings.Blacklist = L(blacklist)
+
+        self.trust_settings:saveSettings(true)
+    else
+        success = false
+        message = "Invalid element "..(element_name or 'nil')..", valid elements are "..self:get_valid_elements():tostring()
+    end
+
+    return success, message
+end
+
+-- // trust mb reset
+function MagicBurstTrustCommands:handle_reset_blacklist()
+    local success
+    local message
+
+    local current_settings = self.trust_settings:getSettings()[state.MainTrustSettingsMode.value].NukeSettings
+    current_settings.Blacklist:clear()
+
+    self.trust_settings:saveSettings(true)
+
+    success = true
+    message = "Magic bursts of all types are enabled"
 
     return success, message
 end
