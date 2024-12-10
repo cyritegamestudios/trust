9d7f09d65f2ebc70d8a3fd0f1b3c15f34274e281
cyrite <123847593+cyrite@users.noreply.github.com>
2024-12-10 00:16:03 -0500

    v12.6.3 (#273)
    * Updating Buffer to work with job names condition
    
    * Updating default job settings
    
    * Converting Edit to Abilities in BuffSettingsMenuItem
    
    * Fixing nil exception
    
    * Fixing main job condition for Buffer
    
    * Bumping Trust version to v12.5.5
    
    * PR feedback
    
    * Fixing issues with alliance assist
    
    * More alliance fixes
    
    * Fixing more alliance issues
    
    * Removing print statements
    
    * Fixing party widget for alliances
    
    * Adding party member distance tracker
    
    * Fixing alliance assist engage
    
    * Fixing alliance assist
    
    * Fixing more edge cases around alliance targeting
    
    * Adding delay before nitro
    
    * PR feedback
    
    * Updating more roles to work with alliance
    
    * Fixing issue where magic bursting Dia would not register in mob debuff trackdr
    
    * Adding ability to ignore party and alliance members from healing
    
    * Fixing more edge cases for alliance healing
    
    * Removing debug code
    
    * PR feedback
    
    * PR feedback
    
    * Moving widget loading to coroutine
    
    * Bumping Trust version to 12.5.6
    
    * Cleanup and updating imports
    
    * Cleanup
    
    * Fixing translations in Japanese
    
    * Bumping Trust version to v12.5.7
    
    * PR feedback
    
    * Fixing localization for PickerConfigItem
    
    * Fixing has buffs
    
    * Fixing various translation and gambit condition issues
    
    * PR feedback
    
    * cleanup
    
    * Fixing JP translation issues in gambit settings editor
    
    * Adding translations for some conditions
    
    * More JP translation fixes
    
    * More translation fixes
    
    * PR feedback
    
    * Fixing issue where player job and sub job were not accurate on addon load
    
    * Fixing issue where bio wasn't properly tracked as an enemy debuff
    
    * Adding fallback handling for when GearSwap is not loaded
    
    * Adding matching for spells and job abilities with no GearSwap or Shortcuts loaded
    
    * More refactoring
    
    * Migration BloodPactWard action
    
    * Refactoring actions to use new command actions
    
    * Bumping Trust version to 12.6.2
    
    * Adding UseItemCommand
    
    * Updating the TrustInstaller
    
    * Bumping Trust version to 12.6.3

--- cylibs/ui/input/chat/commands/game_command.lua
+++ cylibs/ui/input/chat/commands/game_command.lua
@@ -2,6 +2,11 @@ local GameCommand = {}
 GameCommand.__index = GameCommand
 GameCommand.__type = "GameCommand"
 
+-------
+-- Default initializer for a new game command.
+-- @tparam string prefix Command prefix (e.g. /ra, /magic)
+-- @tparam number targetId Target of the command
+-- @treturn GameCommand A game command
 function GameCommand.new(prefix, targetId)
     local self = setmetatable({}, GameCommand)
 
@@ -11,6 +16,9 @@ function GameCommand.new(prefix, targetId)
     return self
 end
 
+-------
+-- Returns the target of the command.
+-- @treturn tuple Target id and index
 function GameCommand:getTargetInfo()
     local target = windower.ffxi.get_mob_by_id(self.targetId)
     if target then
@@ -19,6 +27,12 @@ function GameCommand:getTargetInfo()
     return nil, nil
 end
 
+-------
+-- Returns the text to send to chat to run the command.
+-- @tparam string prefix Command prefix (e.g. /ra, /magic)
+-- @tparam string abilityName Name of spell, job ability, etc
+-- @tparam number targetId Target of the command
+-- @treturn string Localized command
 function GameCommand:getInputText(prefix, abilityName, targetId)
     if abilityName then
         if i18n.current_locale() == i18n.Locale.Japanese then
@@ -35,6 +49,11 @@ function GameCommand:getInputText(prefix, abilityName, targetId)
     end
 end
 
+-------
+-- Runs the command. If `sendInChat` is true, the command will be run by
+-- inputting the command text into the chat. If false, the packet will
+-- be sent directly instead.
+-- @tparam boolean sendInChat Whether to run the command in the chat
 function GameCommand:run(sendInChat)
 end
 
