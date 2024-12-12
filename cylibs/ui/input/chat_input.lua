1e242f33a248f5c1bc764c783562abe9894dd3d9
cyrite <123847593+cyrite@users.noreply.github.com>
2024-12-12 11:45:10 -0800

    PR feedback

--- cylibs/ui/input/chat_input.lua
+++ cylibs/ui/input/chat_input.lua
@@ -23,10 +23,6 @@ function ChatInput.new(addonSettings)
         end
     end
 
-    local testRegex = buildRegex('/ma')
-
-    local _, spellName, targetId = string.match("/ma \"Valor Minuet III\" 12345", testRegex)
-
     self:registerHandler(L{ buildRegex("/song"), buildRegex("/ma"), buildRegex("/magic") }, function(inputText, regex)
         local _, spellName, targetId = string.match(inputText, regex)
 
