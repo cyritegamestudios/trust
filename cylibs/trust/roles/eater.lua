4cd4945c2f7a587d376507731122ff875a7fc248
cyrite <123847593+cyrite@users.noreply.github.com>
2024-12-19 08:39:08 -0800

    Adding Info view to ModesConfigEditor

--- cylibs/trust/roles/eater.lua
+++ cylibs/trust/roles/eater.lua
@@ -1,2 +1,3 @@
 state.AutoFoodMode = M{['description'] = 'Use food', 'Off', 'Auto'}
+state.AutoFoodMode:set_description('Off', "Okay, I'll go on a diet.")
 state.AutoFoodMode:set_description('Auto', "Okay, I'll eat when I'm hungry.")
