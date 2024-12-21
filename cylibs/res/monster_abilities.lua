4286a78563fd3db46b49f7a79f270e83d2f1b403
cyrite <123847593+cyrite@users.noreply.github.com>
2024-12-20 17:27:04 -0800

    Adding Infernal Bulwark to monster_abilities

--- cylibs/res/monster_abilities.lua
+++ cylibs/res/monster_abilities.lua
@@ -2,5 +2,7 @@
 
 return T{
     [2667] = {id=2667,en="Mighty Guard",status=170},
+    [3220] = {id=3220,en="Infernal Bulwark",status=33},
+    [3449] = {id=3449,en="Infernal Bulwark",status=33},
     [4188] = {id=4188,en="Dark Thorn",status=173},
 }, {"id", "en", "status"}
