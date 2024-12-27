86b69201e540eb0264b42aa8ab8763eb8c12b2a1
Aldros <aldros147@gmail.com>
2024-12-27 17:36:21 -0500

    Fix for Magic Burster assist mode
    For Magic Burster, we assign `spell` on L76 as the res.spell,
    we then attempt to reference it on L79 as the spell class to
    get .name rather than .en. This results in a nil spell.

--- cylibs/trust/roles/magic_burster.lua
+++ cylibs/trust/roles/magic_burster.lua
@@ -76,7 +76,7 @@ function MagicBurster:on_add()
             local spell = res.spells[spell_id]
             if spell and S{'Enemy'}:intersection(S(spell.targets)):length() > 0 and S{'BlackMagic', 'BlueMagic'}:contains(spell.type) then
                 if self.job:knows_spell(spell.id) then
-                    self:cast_spell(Spell.new(spell.name))
+                    self:cast_spell(Spell.new(spell.en))
                 else
                     local spell = self:get_spell(Element.new(res.elements[spell.element].en))
                     if spell then
