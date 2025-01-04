72e4a6a391adece6fc2252b8727663fa011a65dc
cyrite <123847593+cyrite@users.noreply.github.com>
2025-01-03 17:55:35 -0800

    Adding ability to rename profile

--- ui/settings/editors/TrustSetsConfigEditor.lua
+++ ui/settings/editors/TrustSetsConfigEditor.lua
@@ -1,5 +1,6 @@
 local ConfigEditor = require('ui/settings/editors/config/ConfigEditor')
 local PickerConfigItem = require('ui/settings/editors/config/PickerConfigItem')
+local TextInputConfigItem = require('ui/settings/editors/config/TextInputConfigItem')
 
 local TrustSetsConfigEditor = setmetatable({}, {__index = ConfigEditor })
 TrustSetsConfigEditor.__index = TrustSetsConfigEditor
@@ -10,10 +11,14 @@ function TrustSetsConfigEditor.new(trustSetName, trustModeSettings, trustSetting
     currentSettings['job_set_name'] = state.MainTrustSettingsMode.value
     currentSettings['sub_job_set_name'] = state.SubTrustSettingsMode.value
     currentSettings['weapon_skill_set_name'] = state.WeaponSkillSettingsMode.value
+    currentSettings['profile_name'] = trustSetName
 
     local trustSet = trustModeSettings:getSettings()[trustSetName]
 
     local configItems = L{
+        TextInputConfigItem.new('profile_name', trustSetName, "Profile Name", function(text)
+            return true
+        end),
         PickerConfigItem.new('job_set_name', trustSet['maintrustsettingsmode'], trustSettings:getSetNames(), nil, "Job Settings"),
         PickerConfigItem.new('sub_job_set_name', trustSet['subtrustsettingsmode'], subJobTrustSettings:getSetNames(), nil, "Sub Job Settings"),
         PickerConfigItem.new('weapon_skill_set_name', trustSet['weaponskillsettingsmode'], weaponSkillSettings:getSetNames(), nil, "Weapon Skill Settings"),
@@ -34,7 +39,13 @@ function TrustSetsConfigEditor.new(trustSetName, trustModeSettings, trustSetting
         trustSet['subtrustsettingsmode'] = newConfigSettings['sub_job_set_name']
         trustSet['weaponskillsettingsmode'] = newConfigSettings['weapon_skill_set_name']
 
-        trustModeSettings:saveSettings(trustSetName, trustSet, true)
+        local newProfileName = newConfigSettings['profile_name']
+        if newProfileName ~= trustSetName then
+            trustModeSettings:saveSettings(newProfileName, trustSet, true)
+            trustModeSettings:deleteSettings(trustSetName)
+        else
+            trustModeSettings:saveSettings(trustSetName, trustSet, true)
+        end
     end), self:onConfigChanged())
 
     return self
