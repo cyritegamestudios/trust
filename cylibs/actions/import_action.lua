f2e7c4bf72666ef79f044e7edea077a13b5e537f
cyrite <123847593+cyrite@users.noreply.github.com>
2025-01-05 07:06:15 -0800

    Removing print

--- cylibs/actions/import_action.lua
+++ cylibs/actions/import_action.lua
@@ -5,11 +5,11 @@ ImportAction.__index = ImportAction
 function ImportAction.new(import_paths, identifier, description)
     local work = coroutine.create(function()
         for i=1, import_paths:length() do
-            print('importing', import_paths[i], os.clock())
+            --print('importing', import_paths[i], os.clock())
             require(import_paths[i])
             coroutine.yield(i >= 23)
         end
-        print('done importing')
+        --print('done importing')
     end)
 
     local self = setmetatable(AsyncAction.new(work, 0.01, identifier, description), ImportAction)
