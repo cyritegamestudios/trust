b2b0560c5fd226a5ec6d2527f8aa2202bdc5ca27
cyrite <123847593+cyrite@users.noreply.github.com>
2025-02-24 18:43:47 -0800

    Updating Dispeler role to use Gambits

---
+++ cylibs/conditions/num_buffs.lua
@@ -0,0 +1,71 @@
+---------------------------
+-- Condition checking whether the target has a specified number of buffs.
+-- @class module
+-- @name NumBuffsCondition
+
+local serializer_util = require('cylibs/util/serializer_util')
+local PickerConfigItem = require('ui/settings/editors/config/PickerConfigItem')
+
+local Condition = require('cylibs/conditions/condition')
+local NumBuffsCondition = setmetatable({}, { __index = Condition })
+NumBuffsCondition.__index = NumBuffsCondition
+NumBuffsCondition.__type = "NumBuffsCondition"
+NumBuffsCondition.__class = "NumBuffsCondition"
+
+function NumBuffsCondition.new(num_buffs, operator)
+    local self = setmetatable(Condition.new(), NumBuffsCondition)
+    self.num_buffs = num_buffs or 1
+    self.operator = operator or Condition.Operator.GreaterThanOrEqualTo
+    return self
+end
+
+function NumBuffsCondition:is_satisfied(target_index)
+    local target = windower.ffxi.get_mob_by_index(self:get_target_index() or target_index)
+    if target then
+        local monster = player.alliance:get_target_by_index(target.index)
+        if monster then
+            return self:eval(L(monster:get_buff_ids()):length(), self.num_buffs, self.operator)
+        else
+            local party_member = player.alliance:get_alliance_member_named(target.name)
+            if party_member then
+                return self:eval(L(party_member:get_buff_ids()):length(), self.num_buffs, self.operator)
+            end
+        end
+    end
+    return false
+end
+
+function NumBuffsCondition:get_config_items()
+    return L{
+        ConfigItem.new('num_buffs', 0, 30, 1, function(value) return value.."" end, "Number of Buffs"),
+        PickerConfigItem.new('operator', self.operator, L{ Condition.Operator.GreaterThanOrEqualTo, Condition.Operator.Equals, Condition.Operator.GreaterThan, Condition.Operator.LessThan, Condition.Operator.LessThanOrEqualTo }, nil, "Operator")
+    }
+end
+
+function NumBuffsCondition:tostring()
+    return string.format("Has %d buffs", self.num_buffs)
+end
+
+function NumBuffsCondition.description()
+    return "Number of buffs."
+end
+
+function NumBuffsCondition.valid_targets()
+    return Condition.TargetType.AllTargets
+end
+
+function NumBuffsCondition:serialize()
+    return "NumBuffsCondition.new(" .. serializer_util.serialize_args(self.num_buffs, self.operator) .. ")"
+end
+
+function NumBuffsCondition:__eq(otherItem)
+    return otherItem.__class == NumBuffsCondition.__class
+            and otherItem.num_buffs == self.num_buffs
+            and otherItem.operator == self.operator
+end
+
+return NumBuffsCondition
+
+
+
+
--- cylibs/trust/roles/dispeler.lua
+++ cylibs/trust/roles/dispeler.lua
@@ -1,14 +1,12 @@
-local buff_util = require('cylibs/util/buff_util')
+local action_message_util = require('cylibs/util/action_message_util')
+local ClaimedCondition = require('cylibs/conditions/claimed')
 local DisposeBag = require('cylibs/events/dispose_bag')
-local job_util = require('cylibs/util/job_util')
-local monster_util = require('cylibs/util/monster_util')
-local JobAbilityAction = require('cylibs/actions/job_ability')
-local StrategemAction = require('cylibs/actions/strategem')
-local WaitAction = require('cylibs/actions/wait')
-local SequenceAction = require('cylibs/actions/sequence')
-local SpellAction = require('cylibs/actions/spell')
+local GambitTarget = require('cylibs/gambits/gambit_target')
+local monster_abilities_ext = require('cylibs/res/monster_abilities')
+local NumBuffsCondition = require('cylibs/conditions/num_buffs')
 
-local Dispeler = setmetatable({}, {__index = Role })
+local Gambiter = require('cylibs/trust/roles/gambiter')
+local Dispeler = setmetatable({}, {__index = Gambiter })
 Dispeler.__index = Dispeler
 
 state.AutoDispelMode = M{['description'] = 'Dispel Enemies', 'Auto', 'Off'}
@@ -22,144 +20,70 @@ state.AutoDispelMode:set_description('Auto', "Okay, I'll try to dispel monster b
 -- @tparam boolean should_retry If true, will attempt to retry dispel on tic
 -- @treturn Dispeler A dispeler role
 function Dispeler.new(action_queue, spells, job_abilities, should_retry)
-    local self = setmetatable(Role.new(action_queue), Dispeler)
-
-    self.spells = (spells or L{}):map(function(spell)
-        spell:get_conditions():append(SpellRecastReadyCondition.new(spell:get_spell().id))
-        return spell
-    end)
-
-    self.job_abilities = (job_abilities or L{}):map(function(job_ability)
-        job_ability:add_condition(JobAbilityRecastReadyCondition.new(job_ability:get_job_ability_name()))
-        return job_ability
-    end)
+    local self = setmetatable(Gambiter.new(action_queue, {}, state.AutoDispelMode), Dispeler)
 
     self.should_retry = should_retry
-    self.check_buffs_cooldown = 6
-    self.last_check_buffs_time = os.time()
-    self.battle_target_dispose_bag = DisposeBag.new()
+
+    local gambit_settings = {
+        Gambits = (spells + job_abilities):map(function(ability)
+            return Gambit.new(GambitTarget.TargetType.Enemy, L{
+                NumBuffsCondition.new(1, Condition.Operator.GreaterThanOrEqualTo),
+                ClaimedCondition.new()
+            }, ability, Condition.TargetType.Enemy)
+        end)
+    }
+    if should_retry then
+        self:set_gambit_settings(gambit_settings)
+    else
+        self.dispel_gambits = gambit_settings.Gambits
+    end
+
+    self.dispose_bag = DisposeBag.new()
 
     return self
 end
 
 function Dispeler:destroy()
-    Role.destroy(self)
+    Gambiter.destroy(self)
 
-    self.battle_target_dispose_bag:destroy()
+    self.dispose_bag:destroy()
 end
 
 function Dispeler:on_add()
-    Role.on_add(self)
-end
+    Gambiter.on_add(self)
 
-function Dispeler:target_change(target_index)
-    Role.target_change(self, target_index)
-
-    self.battle_target_dispose_bag:destroy()
-
-    if target_index then
-        self.battle_target = self:get_party():get_target(monster_util.id_for_index(target_index))
-        if self.battle_target then
-            self.battle_target_dispose_bag:add(self.battle_target:on_gain_buff():addAction(
-                    function (_, target_index, _)
-                        if state.AutoDispelMode.value ~= 'Off' then
-                            self.last_check_buffs_time = os.time()
-                            self:dispel(target_index)
-                        end
-                    end), self.battle_target:on_gain_buff())
-        end
-    end
-end
-
-function Dispeler:tic(_, _)
-    if state.AutoDispelMode.value == 'Off' or not self.should_retry
-            or (os.time() - self.last_check_buffs_time) < self.check_buffs_cooldown
-            or self.battle_target == nil or self.battle_target:get_mob() == nil then
-        return
-    end
-    self.last_check_buffs_time = os.time()
-    self:check_buffs()
-end
-
-function Dispeler:check_buffs()
-    logger.notice("Checking", self.battle_target:get_name(), "for buffs")
-
-    local buff_ids = self.battle_target:get_buff_ids()
-    if buff_ids:length() > 0 then
-        self:dispel(self.battle_target:get_mob().index)
-    end
-end
-
-function Dispeler:dispel(target_index)
-    logger.notice("Dispelling", self.battle_target:get_name())
-
-    if not self.battle_target:is_claimed_by(self:get_alliance()) then
-        return
-    end
-
-    local check_conditions = function(conditions, target_index)
-        for condition in conditions:it() do
-            if not condition:is_satisfied(condition:get_target_index() or target_index) then
-                return false
-            end
-        end
-        return true
-    end
-
-    -- Spells
-    for spell in self.spells:it() do
-        if check_conditions(spell:get_conditions(), target_index) and self.battle_target:get_resist_tracker():numResists(spell:get_spell().id) < 3 then
-            self:cast_spell(spell, target_index)
+    self.dispose_bag:add(WindowerEvents.Ability.Finish:addAction(function(target_id, ability_id)
+        if self:get_target() == nil or self:get_target():get_id() ~= target_id or not ability_id then
             return
         end
-    end
+        local ability = monster_abilities_ext[ability_id]
+        if ability then
+            self:check_gambits(nil, self.dispel_gambits)
+        end
+    end), WindowerEvents.Ability.Finish)
 
-    -- Job abilities
-    for job_ability in self.job_abilities:it() do
-        if check_conditions(job_ability:get_conditions(), target_index) then
-            local target = job_ability:get_target()
-            if target then
-                target_index = target.index
-            end
-            self.action_queue:push_action(JobAbilityAction.new(0, 0, 0, job_ability:get_job_ability_name(), target_index), true)
+    self.dispose_bag:add(WindowerEvents.Action:addAction(function(act)
+        if self:get_target() == nil then
             return
         end
-    end
-end
-
-function Dispeler:cast_spell(spell, target_index)
-    local actions = L{ WaitAction.new(0, 0, 0, 1.5) }
-
-    local can_cast_spell = true
-    for job_ability_name in spell:get_job_abilities():it() do
-        local job_ability = res.job_abilities:with('en', job_ability_name)
-        if can_cast_spell and job_ability and not buff_util.is_buff_active(job_ability.status) then
-            if job_ability.type == 'Scholar' then
-                actions:append(StrategemAction.new(job_ability_name))
-                actions:append(WaitAction.new(0, 0, 0, 1))
-            else
-                if not job_util.can_use_job_ability(job_ability_name) then
-                    can_cast_spell = false
-                else
-                    actions:append(JobAbilityAction.new(0, 0, 0, job_ability_name))
-                    actions:append(WaitAction.new(0, 0, 0, 1))
+        if act.actor_id == self:get_target():get_id() then
+            if action_message_util.is_finish_action_category(act.category) then
+                local action = act.targets[1].actions[1]
+                if action_message_util.is_monster_gain_buff(action.message, action.param) then
+                    self:check_gambits(nil, self.dispel_gambits)
                 end
             end
+
         end
-    end
-    if can_cast_spell then
-        self.last_buff_time = os.time()
+    end), WindowerEvents.Action)
+end
 
-        actions:append(SpellAction.new(0, 0, 0, spell:get_spell().id, target_index, self:get_player()))
-        actions:append(WaitAction.new(0, 0, 0, 2))
+function Dispeler:get_cooldown()
+    return 6
+end
 
-        local dispel_action = SequenceAction.new(actions, 'dispeler'..spell:get_spell().en)
-        dispel_action.priority = ActionPriority.high
-
-        self.action_queue:push_action(dispel_action, true)
-
-        return
-    end
+function Dispeler:allows_multiple_actions()
+    return false
 end
 
 function Dispeler:get_type()
