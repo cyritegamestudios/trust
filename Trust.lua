d9676ad3d4f3078772741e86ab2fc1503af4c115
cyrite <123847593+cyrite@users.noreply.github.com>
2025-02-26 21:40:56 -0800

    Bumping Trust to v13.7.6

--- Trust.lua
+++ Trust.lua
@@ -1,7 +1,7 @@
 _addon.author = 'Cyrite'
 _addon.commands = {'Trust','trust'}
 _addon.name = 'Trust'
-_addon.version = '13.7.5'
+_addon.version = '13.7.6'
 _addon.release_notes = ""
 _addon.release_url = "https://github.com/cyritegamestudios/trust/releases"
 
