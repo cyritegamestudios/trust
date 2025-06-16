fa1ffa1503365403dcd3b6d55368ac14a7766eb6
cyrite <123847593+cyrite@users.noreply.github.com>
2025-06-15 20:55:08 -0700

    Flattening FollowSettingsMenuItem

--- ui/settings/menus/FollowSettingsMenuItem.lua
+++ ui/settings/menus/FollowSettingsMenuItem.lua
@@ -10,9 +10,17 @@ FollowSettingsMenuItem.__index = FollowSettingsMenuItem
 
 function FollowSettingsMenuItem.new(follower, trustModeSettings, addonSettings)
     local self = setmetatable(MenuItem.new(L{
-        ButtonItem.default('Config', 18),
+        ButtonItem.default('Confirm', 18),
         ButtonItem.localized('Modes', i18n.translate('Button_Modes')),
     }, {}, nil, "Following", "Configure follow settings."), FollowSettingsMenuItem)
+
+    self.contentViewConstructor = function(_, _, _)
+        local configItems = L{
+            ConfigItem.new('distance', 1, 18, 1, function(value) return value.." yalms" end, "Follow Distance"),
+            BooleanConfigItem.new('auto_pause', "Pause for Spells and Abilities")
+        }
+        return ConfigEditor.new(self.addonSettings, self.addonSettings:getSettings().follow, configItems)
+    end
 
     self.trustModeSettings = trustModeSettings
     self.addonSettings = addonSettings
@@ -24,7 +32,6 @@ function FollowSettingsMenuItem.new(follower, trustModeSettings, addonSettings)
 end
 
 function FollowSettingsMenuItem:reloadSettings()
-    self:setChildMenuItem("Config", self:getConfigMenuItem())
     self:setChildMenuItem("Modes", self:getModesMenuItem())
 end
 
@@ -44,7 +51,7 @@ end
 
 function FollowSettingsMenuItem:getModesMenuItem()
     return ModesMenuItem.new(self.trustModeSettings, "Set modes for following.",
-            L{'AutoFollowMode', 'IpcMode'})
+            L{'AutoFollowMode'})
 end
 
 return FollowSettingsMenuItem
