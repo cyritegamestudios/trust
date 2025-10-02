37adbb0f1ac6d8185d27247bec7f169d245259c3
cyrite <123847593+cyrite@users.noreply.github.com>
2025-10-02 06:40:51 -0700

    Clean up

--- Trust-Include.lua
+++ Trust-Include.lua
@@ -14,6 +14,5 @@ ReadyMoveSkillSettings = require('settings/skillchains/ReadyMoveSkillSettings')
 TrustAddonSettings = require('settings/TrustAddonSettings')
 TrustSettingsLoader = require('TrustSettings')
 TrustModeSettings = require('TrustModeSettings')
-TrustScenarios = require('scenarios/TrustScenarios')
 TrustSettingsEditor = require('ui/settings/TrustSettingsEditor')
 
