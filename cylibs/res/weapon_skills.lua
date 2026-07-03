0348b29a0050dbac3e3d99c69221d2b360be5386
cyrite <123847593+cyrite@users.noreply.github.com>
2026-07-03 15:55:51 -0700

    Adding defense down to Ageha

--- cylibs/res/weapon_skills.lua
+++ cylibs/res/weapon_skills.lua
@@ -3,5 +3,6 @@ return {
     [85] = {id=85,en="Weapon Break",status={147}},
     [87] = {id=87,en="Full Break",status={146,147,148,149}},
     [115] = {id=115,en="Leg Sweep",status={10}},
+    [155] = {id=155,en="Tachi: Ageha",status={149}},
     [181] = {id=181,en="Shell Crusher",status={149}},
 }, {"id", "en", "status"}
